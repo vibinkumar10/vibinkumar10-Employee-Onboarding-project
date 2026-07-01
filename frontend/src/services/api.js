395d36a029086dd068b9d8ff2c26381a5d742efa
VIBIN KUMAR <vibinkumar@VIBINs-MacBook-Air.local>
2026-07-01 18:20:56 +0530

    Fix login API endpoint

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API = axios.create({
-    baseURL: 'https://vibinkumar10-employee-onboarding.onrender.com'
+    baseURL: 'https://vibinkumar10-employee-onboarding.onrender.com/api'
 })
 API.interceptors.request.use((config) => {
     const token = localStorage.getItem('token')
